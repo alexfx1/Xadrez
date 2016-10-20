cc61976539714b31f53c0ca44a9e8d22e967ce17
Alexandre savastano goiabeira <Alexandre savastano goiabeira>
2016-10-20 21:38:57 -0200

    Implementando as restrições de movimento

--- xadrez/tabuleiro/xadrex/PartidaDeXadrez.cs
+++ xadrez/tabuleiro/xadrex/PartidaDeXadrez.cs
@@ -31,6 +31,26 @@ namespace xadrex {
             turno++;
             mudaJogador();
         }
+
+        public void validarPosicaoDeOrigem(Posicao pos) {
+            if (tab.peca(pos) == null) {
+                throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
+            }
+            if (jogadorAtual != tab.peca(pos).cor) {
+                throw new TabuleiroException("A peça de origem escolhida não é sua!");
+            }
+            if (!tab.peca(pos).existeMovimentosPossiveis()){
+                throw new TabuleiroException("Não há movimentos possíveis para a peça de origem escolhida!");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino) {
+            if (!tab.peca(origem).podeMoverPara(destino)) {
+                throw new TabuleiroException("Posição de destino inválida!");
+            }
+        }
+
+
         private void mudaJogador() {
             if (jogadorAtual == Cor.Branca) {
                 jogadorAtual = Cor.Preta;
